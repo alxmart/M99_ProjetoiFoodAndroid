99683f79c440cc5e32e36405dcf145795017e6f5
alxmart <alxmartinez@gmail.com>
2024-07-19 16:38:27 -0300

    Added 2 activities(Login / Cadastro) and set Login as MAIN

--- cliente/src/main/java/com/luizafmartinez/m99_projetoifoodandroid/LoginActivity.kt
+++ cliente/src/main/java/com/luizafmartinez/m99_projetoifoodandroid/LoginActivity.kt
@@ -5,24 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.m99_projetoifoodandroid.databinding.ActivityLoginBinding
 import com.luizafmartinez.m99_projetoifoodandroid.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate( layoutInflater)
+        ActivityLoginBinding.inflate( layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        inicializar()
-
-    }
-
-    private fun inicializar() {
-
 
     }
 }
